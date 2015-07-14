f3f0987a9fa1be40c5dc234333c4117772560935
0nix <boundlessimagination@gmail.com>
2015-07-14 18:24:14 -0500

    View Experimentation

--- assets/js/run.js
+++ assets/js/run.js
@@ -1,12 +1,25 @@
-/* global Marionette */
 var app = new Marionette.Application();
 app.StaticView = Marionette.ItemView.extend({
-	el:"#main-region",
-	template:"#static-template"
+	//el:"#main-region",
+	template:"#static-template",
+});
+app.RegionContainer = Marionette.LayoutView.extend({
+	el:"#app-container",
+	regions:{
+		main: "#main-region"
+	}
+});
+app.on("before:start",function(){
+	app.regions = new app.RegionContainer();
 });
 app.on("start",function () {
-	console.log("Contact manager started");
+	/*var view = new app.StaticView({
+		template:"#dynamic-template",
+		tagName: "span"
+	});
+	view.render()*/
 	var view = new app.StaticView();
-	view.render();
+	app.regions.main.show(view);
+
 })
 app.start();
